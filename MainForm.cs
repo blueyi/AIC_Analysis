3fb992ad3c23ad72ead43b87c7c0424172cefe53
blueyi <blueyiniu@qq.com>
2020-03-03 01:10:32 +0800

    fix bug about ascii 64

--- MainForm.cs
+++ MainForm.cs
@@ -22,6 +22,32 @@ namespace AIC_ERROR_Analysis
         bool isValidHex(ref string hex, int minLen = 1, int maxLen = 65504)
         {
             hex = System.Text.RegularExpressions.Regex.Replace(hex.ToLower(), "0x", "");
+            char[] all_whitespaces = new char[] {
+                   // SpaceSeparator category
+                   '\u0020', '\u1680', '\u180E', '\u2000', '\u2001', '\u2002', '\u2003',
+                   '\u2004', '\u2005', '\u2006', '\u2007', '\u2008', '\u2009', '\u200A',
+                   '\u202F', '\u205F', '\u3000',
+                   // LineSeparator category
+                   '\u2028',
+                   // ParagraphSeparator category
+                   '\u2029',
+                   // Latin1 characters
+                   '\u0009', '\u000A', '\u000B', '\u000C', '\u000D', '\u0085', '\u00A0',
+                   // ZERO WIDTH SPACE (U+200B) & ZERO WIDTH NO-BREAK SPACE (U+FEFF)
+                   '\u200B', '\uFEFF'};
+            hex = hex.Trim(all_whitespaces);
+            byte[] array = System.Text.Encoding.ASCII.GetBytes(hex);  //数组array为对应的ASCII数组     
+            string tmpStr = null;
+            for (int i = 0; i < array.Length; i++)
+            {
+                int asciiCode = (int)(array[i]);
+                if (asciiCode == 63)
+                {
+                    continue;
+                }
+                tmpStr += hex[i];
+            }
+            hex = tmpStr;
             if (hex.Length > maxLen || hex.Length < minLen)
             {
                 return false;
